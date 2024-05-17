5def77dd3bbc12fe00a6207e79bcca06bb55fd22
EmbraceTheLight <1010642166@qq.com>
2024-05-17 18:26:24 +0800

    define some function prototypes

--- CJSON/CJSON/Unserialization.h
+++ CJSON/CJSON/Unserialization.h
@@ -4,7 +4,9 @@
 
 // parse_object 解析json中的object对象
 Obj* parse_object(char** json_ptr);
+
 // parse_array 解析json中的array对象
 Array* parse_array(char** json_ptr);
+
 // parse_key_value 解析json中的key:value
 JsonValue* parse_key_value(char** json_ptr, JSON* json);
